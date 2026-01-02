5a48631e25f71270604a246027af261ac6425b9a
marciomateus-data-science <mateusrocha8823@gmail.com>
2026-01-02 00:04:00 -0300

    Primeiro commit

---
+++ codigo.py
@@ -0,0 +1,9 @@
+faturamento = 1200 # tipo: int -> número inteiro
+custo = 750.32 # tipo: float -> número com casa decimal
+lucro = faturamento - custo
+
+print("Faturamento foi de ", faturamento)
+print("O custo foi de ", custo)
+print("O lucro foi de ", lucro)
+
+mensagem = "O faturamento da loja foi de x valor" # tipo string -> texto
